c804270f79753a4ff33e2bba89689421479c27af
ksyrin <ksyrin@github.com>
2024-04-21 15:57:10 +0800

    更新为Tab标签导航

---
+++ nScheduler.Web/Shared/BootstrapAppContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+using nScheduler.Common.Extensions;
+using nScheduler.Common.Models;
+
+namespace nScheduler.Web.Shared;
+
+public class BootstrapAppContext
+{
+    public BootstrapAppContext(AuthenticationState auth)
+    {
+        UserName = auth.User.Identity?.Name ?? string.Empty;
+        UserId = auth.User.Claims!.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)!.Value;
+        Email = auth.User.Claims!.FirstOrDefault(x => x.Type == ClaimTypes.Email)!.Value;
+        Phone = auth.User.Claims!.FirstOrDefault(x => x.Type == ClaimTypes.MobilePhone)!.Value;
+        var roleStr = auth.User.Claims!.FirstOrDefault(x => x.Type == ClaimTypes.Role)!.Value;
+        Role = Enum.Parse<UserRole>(roleStr);
+    }
+
+
+    public string UserId { get; init; }
+
+    public string UserName { get; init; }
+
+    public string Email { get; init; }
+
+    public string Phone { get; init; }
+
+    public UserRole Role { get; init; }
+
+    public string UserRole => Role.GetEnumDescription();
+}
+
